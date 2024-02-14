4772b3f545ead0eae1173cc86622f02b11d8c240
Kacper Burza <65431499+kburza@users.noreply.github.com>
2024-02-14 16:28:31 +0100

    Creating SQL database
    - added cursor to execute code
    - SQL database created and data checked

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 import mysql.connector
 
+# Provide mySQL connector with login data (for mydb obj)
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="123password"
 )
 
-print(mydb)
+# Test to see if mySQL database connected (uncomment)
+# print(mydb)
+
+# Initialized database cursor (can now init db commands)
+mycursor =  mydb.cursor()
+
+# Use cursor to exec code (like creating the db)
+# mycursor.execute("CREATE DATABASE testdb")
+
+# check if db was created:
+mycursor.execute("SHOW DATABASES")
+
+for db in mycursor:
+    print(db)
