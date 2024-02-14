6f886926121efe40eda1c073bec2dd752f5dd5b6
Kacper Burza <65431499+kburza@users.noreply.github.com>
2024-02-14 19:14:37 +0100

    SQL Script + more functions
    - added sql script that can be utilized outside of Python program
    - added multiple entries to table + queries (single, multiple) to parse and display data from Python terminal

---
+++ SQL_Script.sql
@@ -0,0 +1,4 @@
+-- USE testdb
+
+SELECT *
+FROM students
--- test.py
+++ test.py
@@ -24,10 +24,56 @@ mycursor =  mydb.cursor()
 #     print(db)
 
 # Creating table
-# mycursor.execute("CREATE TABLE students (name VARCHAR(255), height INTEGER(10))")
+# mycursor.execute("CREATE TABLE students (name VARCHAR(255), age INTEGER(10))")
+
+# Deleting table
+# mycursor.execute("DROP TABLE students")
 
 # Showing created table
-mycursor.execute("SHOW TABLES")
+# mycursor.execute("SHOW TABLES")
+#
+# for tb in mycursor:
+#     print(tb)
 
-for tb in mycursor:
-    print(tb)
+# Populating the database:
+
+# Function to save sql commands
+# sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
+# Data for sql function (tuples)
+
+# single
+# student1 = ("Rachel", 22)
+
+# tuples
+# students = [("Bob", 12),
+#             ("Amanda", 32),
+#             ("Jacob", 21),
+#             ("Avi", 28),
+#             ("Michelle", 17),]
+
+# execute command
+
+# single:
+# mycursor.execute(sqlFormula, student1)
+
+# many:
+# mycursor.executemany(sqlFormula, students)
+
+# Saves changes in db
+# mydb.commit()
+
+# To run command in SQL and grab data
+mycursor.execute("SELECT * FROM students")
+# grab all rows fetched in result (from SQL code)
+# for fetching all entries
+myresult = mycursor.fetchall()
+
+for row in myresult:
+    print(row)
+
+# for fetching one entry
+mycursor.execute("SELECT * FROM students")
+myresult = mycursor.fetchone()
+
+for row in myresult:
+    print(row)
