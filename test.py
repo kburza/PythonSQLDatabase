025d1abd1609cb708218b0817aa51dcb110fea9f
Kacper Burza <65431499+kburza@users.noreply.github.com>
2024-02-14 16:46:09 +0100

    students table added

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="123password"
+    passwd="123password",
+    database="testdb"
 )
 
 # Test to see if mySQL database connected (uncomment)
@@ -17,7 +18,16 @@ mycursor =  mydb.cursor()
 # mycursor.execute("CREATE DATABASE testdb")
 
 # check if db was created:
-mycursor.execute("SHOW DATABASES")
+# mycursor.execute("SHOW DATABASES")
+#
+# for db in mycursor:
+#     print(db)
 
-for db in mycursor:
-    print(db)
+# Creating table
+# mycursor.execute("CREATE TABLE students (name VARCHAR(255), height INTEGER(10))")
+
+# Showing created table
+mycursor.execute("SHOW TABLES")
+
+for tb in mycursor:
+    print(tb)
